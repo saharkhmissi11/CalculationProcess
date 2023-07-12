9dad4d940471021795325661fb960594a056214f
saharkhmissi11 <saharkhmissi90@gmail.com>
2023-07-12 11:00:36 +0200

    first commit

---
+++ CalculationProcessTest.cs
@@ -0,0 +1,39 @@
+﻿using CamundaClient.Dto;
+using CamundaClient;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using CamundaClient.Service;
+
+namespace Calculation
+{
+
+    // First of all, we need to deploy the camunda model on localhost
+    //This class is a test for the workflow (Here we will give the rwo required numbers "x" and "y")
+    //But we can start the process from the camunda tasklist and fill the form there
+    internal class CalculationProcessTest
+    {
+        [Test]
+        public void TestHappyPath()
+        {
+            // Engine client should point to a dedicated Camunda instance for test, preferrably locally available
+            var camunda = new CamundaEngineClient(new System.Uri("http://localhost:8080/engine-rest/engine/default/"), null, null);
+
+            // Deploy the model on the localhost:8080
+            //string deploymentId = camunda.RepositoryService.Deploy("cal", new List<FileParameter> {
+            //FileParameter.FromManifestResource(Assembly.GetExecutingAssembly(), "Calculation.calculation.bpmn") });
+            
+            // Start the process with definitinKey "calculate" with two variables x=21 and y=1000
+            string processInstanceId = camunda.BpmnWorkflowService.StartProcessInstance("calculate", new Dictionary<string, object>()
+            {
+                {"x", 21},
+                {"y", 1000}
+            });
+        }
+    }
+}
---
+++ CalculationWorker.cs
@@ -0,0 +1,25 @@
+﻿
+
+using CamundaClient.Dto; //Contains Data Transfert Object classes for working with camunda entities
+using CamundaClient.Worker; //Contains classes and interfaces to handle external tasks
+
+namespace SimpleCalculationProcess
+{
+    [ExternalTaskTopic("calculate")]  //name of the topic
+    //With this annotation, the system knows that this class  is a camunda task worker
+    [ExternalTaskVariableRequirements("x", "y")] //required variables in order to execute the task worker
+    class CalculationWorker : IExternalTaskAdapter // IExternalTaskAdapter : an interface (from CamundaClient.Worker) that executes the business logic of the task
+    {
+        public void Execute(ExternalTask externalTask, ref Dictionary<string, object> resultVariables)
+        {
+            //ExternalTask : is a class (from CamundaClient.dto) 
+            long x = Convert.ToInt64(externalTask.Variables["x"].Value); // This line of code will take the value the variable "x" of the camunda process "calculate"
+            long y = Convert.ToInt64(externalTask.Variables["y"].Value);
+            long result = x + y;
+            Console.WriteLine("la somme est :" + result);
+            resultVariables.Add("result", result);   // This line of code will put the result in the variable "result " of the process
+
+        }
+
+    }
+}
